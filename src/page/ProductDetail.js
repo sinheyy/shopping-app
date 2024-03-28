fd0a03f4da4f24b7e0d8098037aac07afe5ade1c
sinhey <sinheyyy@gmail.com>
2024-03-28 22:37:32 +0900

    update detail page(in progress)

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Container, Row, Col } from 'react-bootstrap';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 const ProductDetail = () => {
+  let { id } = useParams();   // parameter를 읽어올 수 있음
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    getProductDetail()
+  }, [])
+
+  const getProductDetail = async () => {
+    let url = `https://my-json-server.typicode.com/sinheyy/shopping-app/products/${id}`;
+    let response = await fetch(url);
+    let data = await response.json();
+
+    console.log(data);
+    setProduct(data);
+  }
+
   return (
-    <div>
-      상품 상세 페이지
-    </div>
+    <Container>
+      <Row>
+        <Col className='product-img'>
+          <img src={product?.img} />
+        </Col>
+        <Col>
+          <div>{product?.title}</div>
+          <div>{product?.price}</div>
+
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              SIZE
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
