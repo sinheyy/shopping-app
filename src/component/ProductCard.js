22922963c553a2d8b8e6c0fbd4f6215461b276b2
sinhey <sinheyyy@gmail.com>
2024-03-28 21:34:57 +0900

    update design

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -8,16 +8,20 @@ const ProductCard = ({ item }) => {
     navigate(`/product/${item.id}`);
   }
 
+  const pickIsTrue = () => {
+    return item.choice ? true : false
+  }
+
   return (
     <div className='product-card' onClick={showDetail}>
       <img src={item?.img} />
-      <div><b>{item?.brand}</b></div>
-      <div>{item?.choice == true ? "PICK" : ""}</div>
-      <div>{item?.title}</div>
+      <div className="brandname"><b>{item?.brand}</b></div>
+      <div className={`pickbox${pickIsTrue ? "_switched" : ""}`}>{item?.choice == true ? "[MD PICK]" : ""}</div>
+      <div className='nametext'>{item?.title}<h className='new-text'>{item?.new == true ? "NEW" : ""}</h></div>
       <div className='price'>₩{item?.price}</div>
-      <div className='saleprice'>₩{item?.saleprice} <b className='saleper'>{Math.round((item?.price - item?.saleprice) / item?.price * 100)}%</b></div>
-      <div>{item?.new == true ? "NEW" : ""}</div>
-    </div>
+      <div className='saleprice'>₩{item?.saleprice} 　　<b className='saleper'>{Math.round((item?.price - item?.saleprice) / item?.price * 100)}%</b></div>
+
+    </div >
   )
 }
 
