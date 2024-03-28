377de6bbfee9cad34a248b22225a1fd0b49791be
sinhey <sinheyyy@gmail.com>
2024-03-28 19:40:15 +0900

    edit json url

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -6,7 +6,7 @@ const ProductAll = () => {
   const [productList, setProductList] = useState([]);
 
   const getProducts = async () => {
-    let url = `http://localhost:5000/products`;
+    let url = `https://my-json-server.typicode.com/sinheyy/shopping-app/products`;
     let response = await fetch(url);
     let data = await response.json();
     setProductList(data);
